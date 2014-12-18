1fa529c330100c88978b273cb5c16ed32dfeebdb
Toby Ferguson <toby.h.ferguson@gmail.com>
2014-12-18 05:53:15 -0800

    Modified dhcp setup so that dnsmasq would provide full dhcp capabilities Put hole in firewall to allow dhcp requests through

--- .dhcp/setup.sh
+++ .dhcp/setup.sh
@@ -11,6 +11,9 @@ sudo yum -q -y install ${oem_agent_packages[*]} dnsmasq
 
 # use the configuration file and then restart dnsmasq
 sudo cp -f /vagrant/.dhcp/dnsmasq.conf /etc/dnsmasq.conf
-
 sudo chkconfig --add dnsmasq
 sudo service dnsmasq start
+
+# Update iptables so that the service can be accessed
+sudo iptables -I INPUT -i eth1 -p udp --dport 67 -j ACCEPT
+sudo iptables save
