377d38e6d35a66372e8525658d1dcdd45da30c9e
Toby Ferguson <toby.h.ferguson@gmail.com>
2015-04-16 09:26:34 -0700

    Corrected generation of agent rpm to use a local directory and copy the rpm across for subsequent transfer to the stage server. This fixes the bug where the agent rpm generation would say that the rpm already existed.

--- .oms/generate_agent.sh
+++ .oms/generate_agent.sh
@@ -3,24 +3,35 @@
 AGENT_RPM=oracle-agt-12.1.0.4.0-1.0.x86_64.rpm
 BASE_DIR=/usr/lib/oracle
 SRC_DIR=${BASE_DIR:?}/RPMS/x86_64
+TMP_DIR=/tmp/agentimage
 DEST_DIR=/vagrant
 # ensure directories are available, with correct permissions (this corrects a bug in the OEM installation)
 install --owner oracle -d ${BASE_DIR:?}
 
-install --owner oracle -d ${DEST_DIR:?}
+
+
 usermod -a -G vagrant oracle
 
 if [ -f ${DEST_DIR:?}/${AGENT_RPM:?} ] # does final RPM exist?
 then
     :				       # yes - do nothing
 else
-    if [ -f ${SRC_DIR:?}/${AGENT_RPM:?} ] # no - is there an internal copy?
-    then				# yes - then copy it to the destination directory
-	cp ${SRC_DIR:?}/${AGENT_RPM:?} ${DEST_DIR:?}/${AGENT_RPM:?}
-    else			# no - generate the rpm into the destination directory
-	echo 'Generating agent RPM. This takes around 10 minutes'
-	su -c "emcli get_agentimage_rpm -destination=${DEST_DIR:?} -platform='Linux x86-64'" - oracle
+    if [ -f ${TMP_DIR:?}/${AGENT_RPM:?} ] # no final RPM, is there one in TMP_DIR?
+    then				  # yes - copy it to the dest dir
+	cp ${TMP_DIR:?}/${AGENT_RPM:?} ${DEST_DIR:?}/${AGENT_RPM:?}
+    else
+	if [ -f ${SRC_DIR:?}/${AGENT_RPM:?} ] # not one in TMP_DIR - is there an internal copy?
+	then				# yes - then copy it to the destination directory
+	    cp ${SRC_DIR:?}/${AGENT_RPM:?} ${DEST_DIR:?}/${AGENT_RPM:?}
+	else			# no - generate the rpm into the TMP_DIR and copy to the destination directory
+	    echo 'Generating agent RPM. This takes around 10 minutes'
+	    # Ensure TMP_DIR is empty, and writable by oracle user
+	    rm -rf ${TMP_DIR:?}
+	    install --owner oracle -d ${TMP_DIR:?}
+	    su -c "emcli get_agentimage_rpm -destination=${TMP_DIR:?} -platform='Linux x86-64'" - oracle
+	    cp ${TMP_DIR:?}/${AGENT_RPM:?} ${DEST_DIR:?}
+	fi
+	chown vagrant:vagrant ${DEST_DIR:?}/${AGENT_RPM:?}
     fi
-    chown vagrant:vagrant ${DEST_DIR:?}/${AGENT_RPM:?}
 fi
 
